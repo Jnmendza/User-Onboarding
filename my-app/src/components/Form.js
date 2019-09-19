49bb9fa782081ef119b10e650c74c8ce46167749
Jnmendza <jonatmendoza@gmail.com>
2019-09-19 14:29:29 -0700

    Step 2

--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -3,13 +3,17 @@ import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
-const onBoardForm = ({ values }) => {
+const onBoardForm = ({ values, errors, touched }) => {
     return (
         <div className="on-board-form">
             <Form>
                 <Field type='text' name='name' placeholder='Full Name' />
+                {touched.name && errors.name && (<p className='error'>{errors.name}</p>)}
                 <Field type='text' name='email' placeholder='Email' />
+                {touched.email && errors.email && (<p className='error'>{errors.email}</p>)}
                 <Field type='text' name='password' placeholder='Password' />
+                {touched.password && errors.password && (<p className='error'>{errors.password}</p>)}
+
                 <label>
                     Terms of Service
                     <Field type='checkbox' name='terms' checked={values.terms} />
@@ -28,7 +32,12 @@ const FormikOnBoardForm = withFormik({
             password: password || '',
             terms: terms || ''
         };
-    }
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required('You must add your name'),
+        email: Yup.string().required('You must add your email'),
+        password: Yup.string().required('Please add the password you cant remember'),
+    })
 })(onBoardForm);
 
 export default FormikOnBoardForm;
