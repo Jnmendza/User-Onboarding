52baba909bd1c716162e7b0e66ca8a5d1197181e
Jnmendza <jonatmendoza@gmail.com>
2019-09-19 18:24:00 -0700

    Step 4

--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -3,8 +3,9 @@ import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
-const onBoardForm = ({ values, errors, touched, status }) => {
+const OnBoardForm = ({ values, errors, touched, status }) => {
     const [users, setUsers] = useState([]);
+
     useEffect(() => {
         if (status) {
             setUsers([...users, status]);
@@ -16,42 +17,54 @@ const onBoardForm = ({ values, errors, touched, status }) => {
             <Form>
                 <Field type='text' name='name' placeholder='Full Name' />
                 {touched.name && errors.name && (<p className='error'>{errors.name}</p>)}
-                <Field type='text' name='email' placeholder='Email' />
+
+                <Field type='email' name='email' placeholder='Email' />
                 {touched.email && errors.email && (<p className='error'>{errors.email}</p>)}
-                <Field type='text' name='password' placeholder='Password' />
+
+                <Field type='password' name='password' placeholder='Password' />
                 {touched.password && errors.password && (<p className='error'>{errors.password}</p>)}
 
                 <label>
                     Terms of Service
-                    <Field type='checkbox' name='terms' checked={values.terms} />
+                    <Field type='checkbox' name='checkbox' checked={values.checkbox} />
                 </label>
                 <button type="submit">Submit!</button>
             </Form>
+            {users.map(user => (
+                <ul key={user.id}>
+                    <li>Name:{user.name}</li>
+                    <li>E-mail:{user.email}</li>
+                    <li>Password:{user.password}</li>
+                </ul>
+            ))}
         </div>
     );
 };
 
 const FormikOnBoardForm = withFormik({
-    mapPropsToStatus({name, email, password, terms}) {
+    mapPropsToStatus({name, email, password, checkbox}) {
         return {
             name: name || '',
             email: email || '',
             password: password || '',
-            terms: terms || ''
+            checkbox: checkbox || ''
         };
     },
     validationSchema: Yup.object().shape({
         name: Yup.string().required('You must add your name'),
-        email: Yup.string().required('You must add your email'),
-        password: Yup.string().required('Please add the password you cant remember'),
+        email: Yup.string().email().required('You must add your email'),
+        password: Yup.string().min(9).required('Please add the password'),
     }),
     handleSubmit(values, { setStatus }) {
+        // console.log(values)
         axios
             .post('https://reqres.in/api/users_', values)
             .then(response => {setStatus(response.data); })
             .catch(error => console.log(error.response));
     }
-})(onBoardForm);
+
+
+})(OnBoardForm);
 
 console.log('THIS IS THE HOC', FormikOnBoardForm)
 export default FormikOnBoardForm;
